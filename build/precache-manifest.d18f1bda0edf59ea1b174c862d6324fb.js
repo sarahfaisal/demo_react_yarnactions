2765ea2e3b7495886cc5fc6a211605a1747533b9
sarahfaisal <faisalsara124@gmail.com>
2020-06-10 12:50:49 +0000

    build removed

---
+++ build/precache-manifest.d18f1bda0edf59ea1b174c862d6324fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ebd07a741e8cd7618a83767687a05e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f2517e55216f91cd2c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "049afbcc86a8f09cd14c",
+    "url": "/static/js/2.fa955dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa955dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f2517e55216f91cd2c",
+    "url": "/static/js/main.d1d17812.chunk.js"
+  },
+  {
+    "revision": "8029db20374c889ff1f1",
+    "url": "/static/js/runtime-main.09f21ec6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
